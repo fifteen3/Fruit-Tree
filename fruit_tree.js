93003ae4ee549f2bb7cb534b817974aa55efdf98
Carlo Costantini <carlo@carlocostantini.ca>
2012-03-10 15:54:06 -0500

    add comments and make the type property of the fruit object

--- fruit_tree.js
+++ fruit_tree.js
@@ -1,25 +1,41 @@
-
-var getFruitTree = function (fruits, type) {
-    var fruits = fruits || 10,
+/**
+ * This function will return a Fruit Tree
+ * @param {Number} yield The number of fruits provided will be the available yield of the tree
+ * @param {String} type The type will be the type of fruit the tree provides
+ * @return {Object} returns a fruit tree of the type provided
+ */
+var getFruitTree = function (yield, type) {
+    var fruits = fruits || 3,
+        /**
+         * Create fruit for the tree
+         * @param {String} the type of fruit to create
+         * @return {Object} the fruit object to be returned
+         */
         fruitMaker = function (type) {
             var eaten = false;
             return {
-                    type : type,
-                    eat  :  function(){
-                            if (!eaten) {
-                                eaten = true;
-                                console.log('mmm, good ' + this.type); 
-                            }
-                            else {
-                                console.log('all gone. already ate ' + this.type);
-                            }
-                        }
+                getType : function() {
+                    return type;
+                },
+                eat  :  function(){
+                    if (!eaten) {
+                        eaten = true;
+                        console.log('mmm, good ' + type);
+                    }
+                    else {
+                        console.log('all gone. already ate ' + type);
+                    }
+                }
             };
         },
+        /**
+         * get the fruit object from the tree
+         * @return {Object}
+         */
         getFruit = function () {
             if (fruits < 1){
                 console.log('no more ' + type);
-                return null;  
+                return null;
             }
             fruits--;
             return fruitMaker(type);
